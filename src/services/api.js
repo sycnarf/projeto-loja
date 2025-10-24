8dc661ea88449bd73af970eacb57210c39c114f8
francys <francys.ribeiro14@gmail.com>
2025-10-23 21:06:09 -0300

    Remove JSON-Server dependency and read products from static db.json

--- src/services/api.js
+++ src/services/api.js
@@ -1,41 +1,10 @@
-/* globals fetch */
-const BASE_URL = "http://localhost:3000";
+import db from '../../db.json';
 
 export async function obterProdutos() {
-  const response = await fetch(`${BASE_URL}/produtos`);
-  return await response.json();
+  return db.produtos;
 }
 
 export async function obterProduto(id) {
-  const response = await fetch(`${BASE_URL}/produtos/${id}`);
-  return await response.json();
+  return db.produtos.find(produto => produto.id === id);
 }
 
-export async function criarProduto(produto) {
-  const response = await fetch(`${BASE_URL}/produtos`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(produto),
-  });
-  return await response.json();
-}
-
-export async function atualizarProduto(id, produto) {
-  const response = await fetch(`${BASE_URL}/produtos/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(produto),
-  });
-  return await response.json();
-}
-
-export async function deletarProduto(id) {
-  const response = await fetch(`${BASE_URL}/produtos/${id}`, {
-    method: "DELETE",
-  });
-  return await response.json();
-}
